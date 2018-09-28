840d9176528cf13d8dce4227b41c4fa23a89bc3a
Sandwich <hi.na3s@gmail.com>
2018-09-28 17:56:51 +0200

    fix build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,34 +1,44 @@
+# If there are implementation issues due to non-compliance of patterns, pass in relative paths (from root of typescript project) as positionals.
+
 if test -z '$1'
 then
-  PATH_DOCS=$1
+  PATH_SOURCE=$1
 else
-  PATH_DOCS="./docs/eosio-docs"
+  PATH_SOURCE=./src
 fi
 
 if test -z '$2'
 then
-  PATH_SOURCE=$2
+  PATH_DOCS=$2
 else
-  PATH_SOURCE="./src"
+  PATH_DOCS=./docs
 fi
 
 if test -z '$3'
 then
   PATH_BUILD=$3
 else
-  PATH_BUILD="./docs/build"
+  PATH_BUILD=$PATH_DOCS/build
 fi
+
+if test -z '$4'
+then
+  PATH_DOC_UTIL=$4
+else
+  PATH_DOC_UTIL=$PATH_DOCS/eosio-docs
+fi
+
 
 #generate typedoc into ./docs/build
 ./node_modules/.bin/typedoc $PATH_SOURCE --options typedoc.json
 #copy book.json into new build directory
-cp $PATH_DOCS/book.json $PATH_BUILD/book.json
+cp $PATH_DOC_UTIL/book.json $PATH_BUILD/book.json
 #copy style overrides into new build directory
-cp -R $PATH_DOCS/theme/styles $PATH_BUILD/styles
+cp -R $PATH_DOC_UTIL/theme/styles $PATH_BUILD/styles
 #copy layout overrides into new build directory
-cp -R $PATH_DOCS/theme/layout $PATH_BUILD/layout
+cp -R $PATH_DOC_UTIL/theme/layout $PATH_BUILD/layout
 #copy images into new build directory
-cp -R $PATH_DOCS/theme/images $PATH_BUILD/images
+cp -R $PATH_DOC_UTIL/theme/images $PATH_BUILD/images
 cp -a ./docs/static/. $PATH_BUILD/
 
 #cd to ./docs/build where typedoc has already been generated and run gitbook install/build
