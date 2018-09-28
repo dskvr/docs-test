9f5d5b5ba04823a03a4241bd16d167e7723bbd22
Sandwich <hi.na3s@gmail.com>
2018-09-28 17:48:39 +0200

    working build

--- scripts/build.sh
+++ scripts/build.sh
@@ -29,8 +29,9 @@ cp -R $PATH_DOCS/theme/styles $PATH_BUILD/styles
 cp -R $PATH_DOCS/theme/layout $PATH_BUILD/layout
 #copy images into new build directory
 cp -R $PATH_DOCS/theme/images $PATH_BUILD/images
-cp -af ./docs/static/. $PATH_BUILD/
+cp -a ./docs/static/. $PATH_BUILD/
+
 #cd to ./docs/build where typedoc has already been generated and run gitbook install/build
 cd $PATH_BUILD && ../../node_modules/.bin/gitbook install
-pwd 
-cd ../.. ./node_modules/.bin/gitbook build $PATH_BUILD
+
+cd ../.. && ./node_modules/.bin/gitbook build $PATH_BUILD
