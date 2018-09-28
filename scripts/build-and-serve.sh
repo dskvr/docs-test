96d4b9195e51807ece315d7bb17da1525c90365c
Sandwich <hi.na3s@gmail.com>
2018-09-28 18:33:04 +0200

    final

--- scripts/build-and-serve.sh
+++ scripts/build-and-serve.sh
@@ -4,5 +4,5 @@ PATH_DOCS=${2-"./docs"}
 PATH_BUILD=${3-$PATH_DOCS/build}
 PATH_DOC_UTIL=${4-$PATH_DOCS/eosio-docs}
 
-sh ./build.sh $PATH_SOURCE $PATH_DOCS $PATH_BUILD $PATH_DOC_UTIL
-sh ./scripts/serve.sh $PATH_BUILD
+sh $PATH_DOCS/scripts/build.sh $PATH_SOURCE $PATH_DOCS $PATH_BUILD $PATH_DOC_UTIL
+sh $PATH_DOCS/scripts/serve.sh $PATH_BUILD
