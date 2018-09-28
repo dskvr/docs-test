3c72bbcdb7a390d2216f4e9cf8dffb09fa146e3e
Sandwich <hi.na3s@gmail.com>
2018-09-28 16:43:01 +0200

    increasing compatbility.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
+# -- /path/to/src
+if test -z '$1'
+then
+  PATH_SRC = $1
+else
+  PATH_SRC = "../../src"
+fi
+
+if test -z '$2'
+then
+  PATH_ROOT = $2
+else
+  PATH_DOC_ROOT = ".."
+fi
+
 #generate typedoc into ./docs/build
-./node_modules/.bin/typedoc ../src --options typedoc.json
+./node_modules/.bin/typedoc $PATH --options typedoc.json
 #copy book.json into new build directory
-cp ./book.json ../build/book.json
+cp ./book.json $PATH/build/book.json
 #copy style overrides into new build directory
-cp -R ./theme/styles ./build/styles
+cp -R ./theme/styles $PATH/build/styles
 #copy layout overrides into new build directory
-cp -R ./theme/layouts ./build/layouts
+cp -R ./theme/layouts $PATH/build/layouts
 #copy images into new build directory
-cp -R ./theme/images ./build/images
+cp -R ./theme/images $PATH/build/images
 #cd to ./docs/build where typedoc has already been generated and run gitbook install/build
 cd ./theme/build && ./node_modules/.bin/gitbook install && ./node_modules/.bin/gitbook build .
--- scripts/serve.sh
+++ scripts/serve.sh
@@ -1,1 +1,8 @@
-./node_modules/.bin/gitbook serve ./build
+if test -z '$2'
+then
+  PATH_BUILD = $2
+else
+  PATH_BUILD = "../build"
+fi
+
+./node_modules/.bin/gitbook serve $PATH_BUILD
